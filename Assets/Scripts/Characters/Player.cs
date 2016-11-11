f7b13c5c8c41618e8936dfe0ccf5022b2124dfc9
Willy Collins <wcurtiscollins@gmail.com>
2016-11-10 20:33:05 -0500

    fix tabs and add threshold to joystick (#12)

--- Assets/Scripts/Characters/Player.cs
+++ Assets/Scripts/Characters/Player.cs
@@ -10,72 +10,76 @@ public class Player : Movable {
 	protected Rigidbody2D body;
 	protected Animator animator;
 	protected InputDevice input;
-    private PlayerStates playerState;
-    private Dictionary<PlayerStates, string> stateAnimMap;
+	private PlayerStates playerState;
+	private Dictionary<PlayerStates, string> stateAnimMap;
 
-    // Controls
-    protected PlayerActions actions;
+	// Controls
+	protected PlayerActions actions;
+	private float joystickThreshold = 0.6f;
 
 	// Moving
 	private float force = 20f;
 
-    public void SetInputDevice(int inputDeviceIndex)
-    {
-        init_actions();
-        actions.Device = InputManager.Devices[inputDeviceIndex];
-    }
+	public void SetInputDevice(int inputDeviceIndex)
+	{
+		init_actions();
+		actions.Device = InputManager.Devices[inputDeviceIndex];
+	}
 
-    void init_actions()
-    {
-        if(actions == null)
-            actions = PlayerActions.CreateWithDefaultBindings();
-    }
+	void init_actions()
+	{
+		if(actions == null)
+			actions = PlayerActions.CreateWithDefaultBindings();
+	}
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		body = gameObject.GetComponent<Rigidbody2D>();
 		animator = transform.GetComponent<Animator>();
-        playerState = PlayerStates.idle;
+		playerState = PlayerStates.idle;
 
-        stateAnimMap = new Dictionary<PlayerStates, string>();
-        stateAnimMap.Add(PlayerStates.idle,"idle");
-        stateAnimMap.Add(PlayerStates.walk, "walk");
-    }
+		stateAnimMap = new Dictionary<PlayerStates, string>();
+		stateAnimMap.Add(PlayerStates.idle,"idle");
+		stateAnimMap.Add(PlayerStates.walk, "walk");
+	}
+
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		if (actions.Right.IsPressed || actions.Left.IsPressed)
 			Walk();
 		else
 			Idle();
-    }
+	}
 
-    private void ChangeState(PlayerStates newState)
-    {
-        if (playerState != newState)
-        {
-            playerState = newState;
-            animator.Play(stateAnimMap[newState], -1, 0f);
-        }
-    }
-
-    protected void Idle()
+	private void ChangeState(PlayerStates newState)
 	{
-        ChangeState(PlayerStates.idle);
-        body.velocity = new Vector2(0, body.velocity.y);
-    }
+		if (playerState != newState)
+		{
+			playerState = newState;
+			animator.Play(stateAnimMap[newState], -1, 0f);
+		}
+	}
+
+	protected void Idle()
+	{
+		ChangeState(PlayerStates.idle);
+		body.velocity = new Vector2(0, body.velocity.y);
+	}
 
 	protected void Walk()
 	{
-        ChangeState(PlayerStates.walk);
+		ChangeState(PlayerStates.walk);
 
-		Vector2 forceVector;
+		Vector2 forceVector = body.velocity;
 
-		if (actions.Left.IsPressed)
+		if (actions.Left.IsPressed && actions.Left.Value > joystickThreshold)
 		{
 			forceVector = Vector2.left * force;
 			FaceLeft();
 		} 
-		else {
+		else if (actions.Right.IsPressed && actions.Right.Value > joystickThreshold) {
 			forceVector = Vector2.right * force;
 			FaceRight();
 		}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,30 +3,32 @@ using System.Collections.Generic;
 
 public class GameController : MonoBehaviour {
 
-    public GameObject player_controller;
-    public List<GameObject> players;
+	public GameObject player_controller;
+	public List<GameObject> players;
 
 	// Use this for initialization
-	void Start () {
-        GameObject playersList = GameObject.Find("Players");
-        players.Add((GameObject)Instantiate(player_controller));
-        players.Add((GameObject)Instantiate(player_controller));
+	void Start ()
+	{
+		GameObject playersList = GameObject.Find("Players");
+		players.Add((GameObject)Instantiate(player_controller));
+		players.Add((GameObject)Instantiate(player_controller));
 
-        int i = 0;
-        foreach (var p in players)
-        {
-            // I guess this is standard practice for parenting new gameobjects
-            // to existing gameobjects in the scene
-            p.transform.parent = playersList.transform;
+		int i = 0;
+		foreach (var p in players)
+		{
+			// I guess this is standard practice for parenting new gameobjects
+			// to existing gameobjects in the scene
+			p.transform.parent = playersList.transform;
 
-            // This assigns each player a different input device
-            ((Player)p.GetComponent("Player")).SetInputDevice(i);
-            i++;
-        }
-    }
+			// This assigns each player a different input device
+			((Player)p.GetComponent("Player")).SetInputDevice(i);
+			i++;
+		}
+	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
 	}
 }
