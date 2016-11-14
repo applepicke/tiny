da887a59eee8e61c1907ce337049dec3a0db1ecf
Grant McNeil <grant.mcneil@gmail.com>
2016-11-13 20:23:03 -0500

    Create level from image map (#28)
    load from map

--- Assets/Scripts/Terrain/TerrainGenerator.cs
+++ Assets/Scripts/Terrain/TerrainGenerator.cs
@@ -7,22 +7,17 @@ public class TerrainGenerator : MonoBehaviour {
 
 	public GameObject tile, ladderTile;
 	public Sprite corner, oneSidedVertical, oneSidedHorizontal, threeSided, middle;
+	public Sprite map;
 
 	private int rows, columns;
 	private TileType[,] tileData;
 	private int scaleSize;
 
-	// Use this for initialization
-	void Start () {
+	void LoadFromMap()
+	{ 
+		rows = map.texture.width;
+		columns = map.texture.height;
 
-		// Here we build the terrain
-		// first we will determite the rows/columns needed based on
-		// the size of the terrain generator. This will dictate the size of
-		// tileData.
-
-		scaleSize = 3; // try lowering this or raising it for smaller or bigger tiles. Looks good at 3
-		rows = ((int)transform.localScale.x/ scaleSize);
-		columns = ((int)transform.localScale.y/ scaleSize);
 		tileData = new TileType[rows, columns];
 
 		// Once this is done, we'll fill it with 1s and 0s representing blocks and non-blocks,
@@ -30,35 +25,46 @@ public class TerrainGenerator : MonoBehaviour {
 		{
 			for (int j = 0; j < columns; j++)
 			{
-				tileData[i,j] = TileType.ground;
+				Color pixelColor = map.texture.GetPixel(i, j);
+				if (pixelColor == new Color(1, 1, 1))
+					tileData[i, j] = TileType.ground;
+				else if (pixelColor == new Color(1, 0, 0))
+					tileData[i, j] = TileType.ladder;
+				else if(pixelColor == new Color(0, 0, 0))
+					tileData[i, j] = TileType.none;
+				else
+				{
+					tileData[i, j] = TileType.none;
+					print("non-assigned color found");
+					print(pixelColor);
+				}
 			}
 		}
+	}
 
-		int halfRow = rows / 2;
-		for (int j = 0; j < columns; j++)
-		{
-			tileData[halfRow, j] = TileType.ladder;
-		}
-
+	void InstanceTerrain()
+	{
 		// finally, iterate over the tileData creating instances of the Tile prefab, setting the sprite
 		// and flipping it according to what tiles are surrounding the current tile.
-		for (int i=0; i<rows; i++)
+		for (int i = 0; i < rows; i++)
 		{
 			for (int j = 0; j < columns; j++)
 			{
-				GameObject toInstantiate;
+				GameObject toInstantiate = null;
 
 				if (tileData[i, j] == TileType.ground)
 					toInstantiate = tile;
-				else
+				else if (tileData[i, j] == TileType.ladder)
 					toInstantiate = ladderTile;
+				else if (tileData[i, j] == TileType.none)
+					continue;
 
-				GameObject newTile = (GameObject)Instantiate(toInstantiate, new Vector3((i * scaleSize) - ((rows*scaleSize) / 2) + transform.position.x, (j * scaleSize) - ((columns*scaleSize) / 2) + transform.position.y, 0), Quaternion.identity);
+				GameObject newTile = (GameObject)Instantiate(toInstantiate, new Vector3((i * scaleSize) - ((rows * scaleSize) / 2) + transform.position.x, (j * scaleSize) - ((columns * scaleSize) / 2) + transform.position.y, 0), Quaternion.identity);
 				SpriteRenderer tileRenderer = ((SpriteRenderer)newTile.GetComponent("SpriteRenderer"));
 
 				newTile.transform.localScale = new Vector3(scaleSize, scaleSize, 1);
 
-				if (tileData[i, j] == TileType.ground)	// normal ground
+				if (tileData[i, j] == TileType.ground)  // normal ground
 				{
 					if (isSolid(i - 1, j) && isSolid(i + 1, j) && isSolid(i, j - 1) && isSolid(i, j + 1))   // surrounded by solid?
 						tileRenderer.sprite = middle;
@@ -107,12 +113,25 @@ public class TerrainGenerator : MonoBehaviour {
 				newTile.transform.parent = transform;
 			}
 		}
+	}
 
+	// Use this for initialization
+	void Start () {
+		// Here we build the terrain
+		// first we will determite the rows/columns needed based on
+		// the size of the terrain generator. This will dictate the size of
+		// tileData.
+		scaleSize = 3; // try lowering this or raising it for smaller or bigger tiles. Looks good at 3
+
+		// Create TileData from Image map
+		LoadFromMap();
+		// Create instances of tiles
+		InstanceTerrain();
 	}
 
 	bool isSolid(int row, int column)
 	{
-		if (row < 0 || row > rows - 1 || column < 0 || column > columns - 1 || tileData[row,column] == 0)
+		if (row < 0 || row > rows - 1 || column < 0 || column > columns - 1 || tileData[row,column] == TileType.none || tileData[row, column] == TileType.ladder)
 			return false;
 
 		return true;
